4fd911926ec60617b91244927b8a62884c2a6e81
gexamb <30425186+gexamb@users.noreply.github.com>
2020-04-08 20:57:12 -0700

    Update osDeployWin10.bat

--- osDeployWin10.bat
+++ osDeployWin10.bat
@@ -6,8 +6,4 @@ powershell.exe -ExecutionPolicy Bypass -File ".\osdeployWin10.ps1" -patch 1
 
 VMwareOSOptimizationTool.exe -o -t .\CSUN_WinSys_Win10.xml -v > osotlog.txt 2>&1
 
-REM ********* Press any key to restart *********
-
-pause
-
-shutdown /f /r /t 0
+shutdown /f /r /t 10
